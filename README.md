5e7591a532593cece3da8a89d9fbc758bef00eab
yarkocode <yao6lav.velicko@gmail.com>
2025-09-10 12:53:19 +0700

    initialize laravel app

--- README.md
+++
@@ -1,2 +0,0 @@
-# ttgdttech-newsapp
-This is a project by students, for students, who're learning website administration
---
+++ resources/js/types/index.d.ts
@@ -0,0 +1,19 @@
+export interface Auth {
+    user: User;
+}
+
+export type AppPageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
+    name: string;
+    quote: { message: string; author: string };
+    auth: Auth;
+};
+
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    avatar?: string;
+    email_verified_at: string | null;
+    created_at: string;
+    updated_at: string;
+}
